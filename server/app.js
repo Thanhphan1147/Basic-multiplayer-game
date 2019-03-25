0f2d5bbd46ace9ca63c8a2681b693b349f6290d1
Phan Trung Thanh <phantrungthanh25@gmail.com>
2019-03-25 22:45:18 +0100

    Update app.js
    added line collision function

--- server/app.js
+++ server/app.js
@@ -49,6 +49,27 @@ function ColisionDetector(key) {
     return false;
 }
 
+function LineCollision(p1, p2, p3, p4) {
+    var d = ((p4.y - p3.y) * (p2.x - p1.x)) - ((p4.x - p3.x) * (p2.y - p1.y));
+    var n1 = ((p4.x - p3.x) * (p1.y - p3.y)) - ((p4.y - p3.y) * (p1.x - p3.x));
+    var n2 = ((p2.x - p1.x) * (p1.y - p3.y)) - ((p2.y - p1.y) * (p1.x - p3.x));
+
+    if ( d == 0.0 )
+    {
+        if ( n1 == 0.0 && n2 == 0.0 )
+        {
+            return false;  //COINCIDENT;
+        }
+
+        return false;   // PARALLEL;
+    }
+
+    var ua = n1 / d;
+    var ub = n2 / d;
+    //console.log(p1.x + ',' + p1.y + ',' + p2.x + ',' + p2.y);
+    return (ua >= 0.0 && ua <= 1.0 && ub >= 0.0 && ub <= 1.0);
+}
+
 initPlayers();
 //-----------------------//
 app.use(express.static('public'));
