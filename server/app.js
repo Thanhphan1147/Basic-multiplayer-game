8957d4961d3dc53072d584c707f2f489734a7f41
Phan Trung Thanh <phantrungthanh25@gmail.com>
2019-03-31 23:15:42 +0200

    Separate client and server

--- server/app.js
+++ server/app.js
@@ -72,10 +72,12 @@ function LineCollision(p1, p2, p3, p4) {
 
 initPlayers();
 //-----------------------//
+/*
 app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html")
 })
+*/
 
 io.on('connection', (socket) => {
     console.log('new user connected');
